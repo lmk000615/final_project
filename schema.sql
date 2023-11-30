4c0004e5423d019d67e5f2460a678bf169261048
Charlotte0501 <lc3813@columbia.edu>
2023-11-29 22:53:47 -0500

    part1 update2
    rents data & zipcode clean&filter update. Still need to normalize SRID & column types.

--- schema.sql
+++ schema.sql
@@ -1,36 +1,36 @@
 
-            CREATE TABLE zip_codes (
-                zip_code Integer PRIMARY KEY,
-                neighborhood String,
-                borough String,
-                geometry GEOMETRY(Point, 4326)
-            );
+        CREATE TABLE zip_codes (
+            zip_code Integer PRIMARY KEY,
+            neighborhood TEXT,
+            borough TEXT,
+            geometry GEOMETRY(Point, 4326)
+        );
 
-            CREATE TABLE complaints (
-                complaint_id Integer PRIMARY KEY,
-                created_date TIMESTAMP,
-                complaint_type String,
-                descriptor String,
-                zip_code Integer REFERENCES zip_codes(zip_code),
-                latitude FLOAT,
-                longitude FLOAT,
-                geometry GEOMETRY(Point, 4326)
-            );
+        CREATE TABLE complaints (
+            complaint_id Integer PRIMARY KEY,
+            created_date TIMESTAMP,
+            complaint_type TEXT,
+            descriptor TEXT,
+            zip_code Integer REFERENCES zip_codes(zip_code),
+            latitude FLOAT,
+            longitude FLOAT,
+            geometry GEOMETRY(Point, 4326)
+        );
 
-            CREATE TABLE trees (
-                tree_id Integer PRIMARY KEY,
-                species String,
-                diameter_inches FLOAT,
-                health String,
-                zip_code Integer REFERENCES zip_codes(zip_code),
-                latitude FLOAT,
-                longitude FLOAT,
-                geometry GEOMETRY(Point, 4326)
-            );
+        CREATE TABLE trees (
+            tree_id Integer PRIMARY KEY,
+            species TEXT,
+            diameter_inches FLOAT,
+            health TEXT,
+            zip_code Integer REFERENCES zip_codes(zip_code),
+            latitude FLOAT,
+            longitude FLOAT,
+            geometry GEOMETRY(Point, 4326)
+        );
 
-            CREATE TABLE average_rents (
-                zip_code Integer PRIMARY KEY,
-                year INT,
-                avg_rent DECIMAL
-            );
-        
+        CREATE TABLE average_rents (
+            zip_code Integer PRIMARY KEY,
+            year INT,
+            avg_rent DECIMAL
+        );
+    
